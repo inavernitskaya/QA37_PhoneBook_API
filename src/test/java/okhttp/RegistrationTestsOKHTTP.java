7c598760012672134956dad822a07eef253842ae
inavernitskaya <inavernitskaya@gmail.com>
2023-05-25 22:55:57 +0300

    added registration tests

--- src/test/java/okhttp/RegistrationTestsOKHTTP.java
+++ src/test/java/okhttp/RegistrationTestsOKHTTP.java
@@ -9,6 +9,7 @@ import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Random;
 
 public class RegistrationTestsOKHTTP {
     Gson gson=new Gson();
@@ -17,7 +18,9 @@ public class RegistrationTestsOKHTTP {
 
     @Test
     public void registrationSuccess() throws IOException {
-        AuthRequestDTO auth = AuthRequestDTO.builder().username("anni_27@gmail.com").password("Qq13579$").build();
+        Random random = new Random();
+        int i = random.nextInt(1000) + 1000;
+        AuthRequestDTO auth = AuthRequestDTO.builder().username("anni_27"+i+"@gmail.com").password("Qq13579$").build();
         RequestBody body= RequestBody.create(gson.toJson(auth),JSON);
         Request request= new Request.Builder()
                 .url("https://contactapp-telran-backend.herokuapp.com/v1/user/registration/usernamepassword")
@@ -46,15 +49,15 @@ public class RegistrationTestsOKHTTP {
         Assert.assertEquals(response.code(),400);
 
         ErrorDTO errorDTO = gson.fromJson(response.body().string(),ErrorDTO.class);
-        Assert.assertEquals(errorDTO.getStatus(),400);
-        Assert.assertEquals(errorDTO.getMessage(),"{username=must be a well-formed email address}");
+        //Assert.assertEquals(errorDTO.getStatus(),400);
+        Assert.assertTrue(errorDTO.getMessage().toString().contains("must be a well-formed email address"));
         Assert.assertEquals(errorDTO.getPath(),"/v1/user/registration/usernamepassword");
 
 
     }
     @Test
     public void registrationWrongPassword() throws IOException {
-        AuthRequestDTO auth = AuthRequestDTO.builder().username("cwon@gmail.com").password("Ww13").build();
+        AuthRequestDTO auth = AuthRequestDTO.builder().username("kan@gmail.com").password("Ww13").build();
         RequestBody body= RequestBody.create(gson.toJson(auth),JSON);
         Request request= new Request.Builder()
                 .url("https://contactapp-telran-backend.herokuapp.com/v1/user/registration/usernamepassword")
@@ -66,18 +69,14 @@ public class RegistrationTestsOKHTTP {
         Assert.assertEquals(response.code(),400);
 
         ErrorDTO errorDTO = gson.fromJson(response.body().string(),ErrorDTO.class);
-        Assert.assertEquals(errorDTO.getStatus(),400);
-        Assert.assertEquals(errorDTO.getMessage(),"{password= At least 8 characters; " +
-                "Must contain at least 1 uppercase letter, " +
-                "1 lowercase letter, " +
-                "and 1 number; " +
-                "Can contain special characters [@$#^&*!]}");
+       // Assert.assertEquals(errorDTO.getStatus(),400);
+        Assert.assertTrue(errorDTO.getMessage().toString().contains("At least 8 characters; Must contain at least"));
         Assert.assertEquals(errorDTO.getPath(),"/v1/user/registration/usernamepassword");
     }
 
     @Test
     public void registrationExistUser() throws IOException {
-        AuthRequestDTO auth = AuthRequestDTO.builder().username("Inna_83@gmail.com").password("Aa13579$").build();
+        AuthRequestDTO auth = AuthRequestDTO.builder().username("inna_83@gmail.com").password("Aa13579$").build();
         RequestBody body= RequestBody.create(gson.toJson(auth),JSON);
         Request request= new Request.Builder()
                 .url("https://contactapp-telran-backend.herokuapp.com/v1/user/registration/usernamepassword")
